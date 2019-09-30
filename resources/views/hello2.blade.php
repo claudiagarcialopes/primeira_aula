a51dce594ead74289f4f72e18c84bb48423ee888
Aluno <claudialopes@pbh.gov.br>
2019-09-30 16:59:36 -0300

    first commit

---
+++ resources/views/hello2.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+</html>
+     <head>
+         <title>Hello world 2</title>
+     </head>
+
+     <body>
+         <h1> This is hello world 2<?php echo(2 +2);?></h1>
+     </body>
+</html>
